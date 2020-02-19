f81c7441c977d6fc46de34ff788a6413737e331d
andrea <pacifico.mensa@gmail.com>
2020-02-19 10:44:17 +0100

    up

--- dist/app.js
+++ dist/app.js
@@ -10706,15 +10706,17 @@ return jQuery;
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
 $(document).ready(function () {
+  // hover su solutions
   $("#solutions").hover(function () {
     $(this).children(".dropdown").removeClass("hidden");
   }, function () {
     $(this).children(".dropdown").addClass("hidden");
   });
   $(document).on("click", function (event) {
+    // click su bars
     if ($(event.target).hasClass("fa-bars")) {
       $(event.target).toggleClass("fa-bars fa-times");
-      $(event.target).parents("#navbar").children(".bars_dropdown").removeClass("hidden");
+      $(event.target).parents("#navbar").children(".bars_dropdown").removeClass("hidden"); // click su X
     } else if ($(event.target).hasClass("fa-times")) {
       $(event.target).toggleClass("fa-bars fa-times");
       $(event.target).parents("#navbar").children(".bars_dropdown").addClass("hidden");
--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 let $ = require('jquery');
 
 $(document).ready(function() {
+  // hover su solutions
   $("#solutions").hover(function() {
     $(this).children(".dropdown").removeClass("hidden");
   }, function() {
     $(this).children(".dropdown").addClass("hidden");
   });
   $(document).on("click", function(event) {
+    // click su bars
     if ($(event.target).hasClass("fa-bars")) {
       $(event.target).toggleClass("fa-bars fa-times");
       $(event.target).parents("#navbar").children(".bars_dropdown").removeClass("hidden");
+    // click su X
     } else if ($(event.target).hasClass("fa-times")) {
       $(event.target).toggleClass("fa-bars fa-times");
       $(event.target).parents("#navbar").children(".bars_dropdown").addClass("hidden");
